aaa8f187bbe7dda8cf84e2c56911c798dc7b5bd9
Tonye Jack <tonye@coherentpath.com>
2020-03-23 21:13:20 -0400

    Added support for runnig on specific files.

---
+++ bin/typescript-eslint.js
@@ -0,0 +1,72 @@
+#! /usr/bin/env node
+
+'use strict';
+const minimatch = require('minimatch');
+const shell = require('shelljs');
+
+const argv = require('yargs')
+  .option(
+    'config',
+    {
+      alias: 'c',
+      required: true,
+      type: 'string',
+      description: 'Config file'
+    },
+  )
+  .option(
+    'extensions',
+    {
+      alias: 'ext',
+      required: false,
+      default: 'ts,tsx',
+      type: 'string',
+      description: 'Comma separated file extensions i.e -ext \'ts,tsx,js,jsx\''
+    },
+  )
+  .option(
+    'verbose',
+    {
+      alias: 'v',
+      default: false,
+      type: 'boolean',
+      description: 'Turn on/off verbose output'
+    },
+  )
+  .command(
+    '$0 [filenames..]',
+    'Runs typescript-eslint based on changes to the provided extension files',
+  )
+  .help()
+  .argv;
+
+const { filenames = [], verbose = false } = argv;
+const extensions = argv.extensions.split(',').join('|');
+const fileMatches = filenames.filter(
+  minimatch.filter(`*.+({${extensions})`, { matchBase: true }),
+);
+const shouldExecute = fileMatches.length > 0;
+
+if (!shouldExecute) {
+  if (verbose) {
+    console.log('No matching files found requires: js(x)|ts(x)');
+  }
+  return 0;
+} else {
+  if (verbose) {
+    console.log(`Matching files found: ${fileMatches.join(", ")}`);
+  }
+  const basePath = shell.pwd().toString();
+
+  shell.cd(basePath);
+  
+  const child = shell.exec(`npx eslint --config ${argv.config} --max-warnings=0`, {
+    async: true,
+    verbose
+  });
+  
+  child.stderr.on('data', function (data) {
+    console.error(data);
+    shell.exit(1);
+  });
+}
